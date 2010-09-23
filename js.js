a120a4456e4182aa90882ef3b8e3172f66e4b635
warsus <Zerstroyer@gmail.com>
2010-09-23 21:47:49 +0200

    fixed links

--- js.js
+++ js.js
@@ -2,6 +2,27 @@ $(document).ready(function() {
     alert("shortkeys: a -> focus commentary s -> toggle horizontal/vertical layout d -> show both f -> show source")
     var vertical = true;
     var alignment = "cols";
+    
+    var focusDoc = function () {
+	    $("frameset").attr(alignment,"100%,0%")
+    }
+    var focusSrc = function () {
+	    $("frameset").attr(alignment,"0%,100%")
+    }
+    var focusBoth = function () {
+	$("frameset").attr(alignment,"50%,50%")
+    }
+    var switchAlignment = function () {
+	$("frameset").removeAttr(alignment)
+	vertical = !vertical;
+	if (vertical){
+	    alignment="cols";
+	}
+	else{
+	    alignment="rows";
+	}
+	$("frameset").attr(alignment,"50%,50%")
+    }
 
     $("frame").dblclick(function() {
 	$("frameset").attr(alignment,"80%,20%")
@@ -9,24 +30,16 @@ $(document).ready(function() {
     $("*").keydown(function(e) {
 //	console.log("fsdfsd" + e.which);
 	if(e.which == 65){
-	    $("frameset").attr(alignment,"100%,0%")
+	    focusDoc();
 	}
 	if(e.which == 70){
-	    $("frameset").attr(alignment,"0%,100%")
+	    focusSrc();
 	}
 	if(e.which == 83){
-	    $("frameset").attr(alignment,"50%,50%")
+	    focusBoth();
 	}
 	if(e.which == 68){
-	    $("frameset").removeAttr(alignment)
-	    vertical = !vertical;
-	    if (vertical){
-		alignment="cols";
-	    }
-	    else{
-		alignment="rows";
-	    }
-	    $("frameset").attr(alignment,"50%,50%")
+	    switchAlignment();
 	}
     });
     $("a dt").hover(function() {
