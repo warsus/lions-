4685f54079c05709ada25a3a3d668308a9137571
warsus <zerstroyer@gmail.com>
2026-04-06 22:07:41 +0200

    helper script for future reference

---
+++ helpers/util.ps1
@@ -0,0 +1,1 @@
+Get-ChildItem -File -Recurse | Where-Object { $($_ | Get-Content | Select-Object -First 1) -eq "#"} | ForEach-Object {$a = $_ | get-content ; $b = $a | Select-Object -Skip 1; Set-Content $_ -Value $b -WhatIf}
